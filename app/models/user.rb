5772cbc6003502f5e7a05c13b14174540b7ebb9d
bria_white <bria.awhite@gmail.com>
2020-12-18 15:37:06 -0500

    relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+   has_many :destinations
+   has_many :comments 
+   has_many :commented_destinations, through: :comments, source: :destination
+   has_secure_password
 end
