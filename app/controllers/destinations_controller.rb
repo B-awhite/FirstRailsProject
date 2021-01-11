3e1cfd33a8b9c400ad912f63da0c1a3ffbaca4c6
bria_white <bria.awhite@gmail.com>
2021-01-11 15:32:19 -0500

    New Destinations new, create, index views

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,3 +1,22 @@
 class DestinationsController < ApplicationController
    before_action :if_not_logged_in_redirect
+
+   def new 
+      @destination = Destination.new
+   end 
+
+   def create
+      @destination = current_user.destinations.build(destination_params)
+      if @destination.save 
+         redirect_to destinations_path 
+      else 
+         render :new 
+      end 
+   end 
+
+   private 
+
+   def destination_params
+      params.require(:destination).permit(:location, :city)
+   end 
 end
