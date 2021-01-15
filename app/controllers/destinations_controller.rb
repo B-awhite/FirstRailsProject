19a193ac45acabca6a1529886675318c7276974d
bria_white <bria.awhite@gmail.com>
2021-01-15 13:44:38 -0500

    fixed destination cont/view & refactor

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,7 @@
 class DestinationsController < ApplicationController
    before_action :if_not_logged_in_redirect
-   before_action :finding_user, only: [:new, :index]
+   before_action :destination_search, only: [:show, :edit, :update]
+   before_action :destination_writer?, only: [:edit, :update]
 
    def new 
      if params[:user_id] && @user = User.find_by_id(params[:user_id])
@@ -15,32 +16,42 @@ class DestinationsController < ApplicationController
          @destinations = @user.destinations.alpha
       else 
         @error = "User not found"
-        @destinations = Destination.alpha
+        @destinations = Destination.alpha if params[:user_id]
       end 
    end 
 
    def create
       @destination = current_user.destinations.build(destination_params)
       if @destination.save 
-         redirect_to destinations_path 
+         redirect_to destination_path(@destination) 
       else 
          render :new 
       end 
    end 
 
-   # def index
-   #   if params[:user_id] && @user = User.find_by_id(params[:user_id])
-   #      @destinations = @user.destinations
-   #   else 
-   #      @error = "Destination not found" if params[:user_id]
-   #      @destinations = Destination.all 
-   #   end 
-   # end  
+    def index
+     if params[:user_id] && @user = User.find_by_id(params[:user_id])
+        @destinations = @user.destinations
+     else 
+        @error = "Destination not found" if params[:user_id]
+        @destinations = Destination.all 
+     end 
+   end  
+
 
    def show 
-      @destination = Destination.find_by_id(params[:id])
-      redirect_to destinations_path if !@destination
    end
+
+   def edit
+   end
+  
+    def update
+      if @destination.update(destination_params)
+        redirect_to destination_path(@destination)
+      else
+        render :edit
+      end
+    end
 
    private 
 
@@ -48,9 +59,16 @@ class DestinationsController < ApplicationController
       params.require(:destination).permit(:location, :city)
    end 
 
-   def finding_user
-     if params[:user_id] && @user = User.find_by_id(params[:user_id])
-     end 
-   end 
+   def destination_search
+      @destination = destination.find_by(id: params[:id])
+      if !@destination
+        flash[:message] = "destination not found"
+        redirect_to destinations_path 
+      end 
+    end
+
+    def destination_writer?
+      redirect_to destinations_path if @destination.user != current_user
+    end 
 
 end
